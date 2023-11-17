a866a907ae5180a1536fc60aa7920f7dd56ac69e
Christian Mallier <cmallier@outlook.com>
2023-11-16 20:37:31 -0500

    Updates

--- Samples/OwnedEntityTypes/OwnedEntityTypesApp/AppDbContext.cs
+++ Samples/OwnedEntityTypes/OwnedEntityTypesApp/AppDbContext.cs
@@ -7,16 +7,14 @@ namespace StaticApp;
 public class AppDbContext : DbContext
 {
     protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
-        => optionsBuilder
-          .UseSqlServer( @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=OwnedEntityTypesApp; Integrated Security=True; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False" )
-          .LogTo( Console.WriteLine, new[] { RelationalEventId.CommandExecuted } )
-          .EnableSensitiveDataLogging();
-
+    {
+        optionsBuilder.UseSqlServer( @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=OwnedEntityTypesApp; Integrated Security=True; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False" )
+                      .LogTo( Console.WriteLine, new[] { RelationalEventId.CommandExecuted } )
+                      .EnableSensitiveDataLogging();
+    }
 
     // DbSets
     public DbSet<Order> Orders => Set<Order>();
-
-
 
     protected override void OnModelCreating( ModelBuilder modelBuilder )
     {
@@ -25,8 +23,7 @@ public class AppDbContext : DbContext
 
         //modelBuilder.Entity<Order>()
         //            .OwnsOne( typeof( StreetAddress ), "ShippingAddress" );
-
-        //modelBuilder.Entity<Order>().OwnsOne(
+        //modelBuilder.Entity<Order>().OwnsOne(
         //   o => o.ShippingAddress,
         //   sa =>
         //   {
